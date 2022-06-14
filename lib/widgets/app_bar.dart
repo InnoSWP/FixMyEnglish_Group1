4dcbe2de71ee6f0baadf8da0b40673e0264064e7
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-06-14 15:15:09 +0300

    added getAppBar() function

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
 
-class MyAppBar extends StatelessWidget {
-  const MyAppBar({super.key});
+import '../constants/constants.dart';
 
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      elevation: 0,
-      backgroundColor: const Color.fromARGB(255, 136, 165, 80),
-      title: const Text('iExtract'),
-    );
-  }
+AppBar getAppBar({
+  backgroundColor = const Color(0xFF4D6658),
+  title,
+  elevation = 0,
+}) {
+  title ??= getLogo();
+  return AppBar(
+    elevation: elevation,
+    backgroundColor: backgroundColor,
+    title: title,
+  );
+}
+
+Widget getLogo({
+  width = 200,
+  image,
+}) {
+  image ??= getImage(
+    message: 'trying to get Logo',
+    path: logoPath,
+    width: width,
+  );
+  return Container(
+    alignment: Alignment.center,
+    child: image,
+  );
+}
+
+Image getImage({
+  message,
+  required path,
+  width,
+}) {
+  return Image(
+    image: AssetImage(path),
+    color: Colors.white,
+    width: width,
+    errorBuilder:
+        (BuildContext context, Object exception, StackTrace? stackTrace) =>
+            Text('error: [$message] not found!'),
+  );
 }
