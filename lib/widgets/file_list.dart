30bd10e1bbe5609c02e3a1d75614671af61666d6
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-06-14 15:17:58 +0300

    split into two widgets[file_info.dart]

--- lib/widgets/file_list.dart
+++ lib/widgets/file_list.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 
-class File {
-  String? name;
-  int id = 0;
-  File({required this.name, required this.id});
-}
+import '../models/file.dart';
+import './file_info.dart';
 
 class FileListView extends StatefulWidget {
   const FileListView({super.key});
@@ -14,7 +11,7 @@ class FileListView extends StatefulWidget {
 }
 
 class _FileListViewState extends State<FileListView> {
-  final files = <File>[
+  static final _files = <File>[
     File(name: 'file1', id: 1),
     File(name: 'file2', id: 2),
     File(name: 'file3', id: 3),
@@ -27,13 +24,17 @@ class _FileListViewState extends State<FileListView> {
   ];
 
   void removeFile({int? id, File? file}) {
-    if (file != null && files.contains(file)) {
-      files.remove(file);
+    if (file != null && _files.contains(file)) {
+      setState(() {
+        _files.remove(file);
+      });
       print('file with id: ${file.id} removed!');
     } else if (id != null) {
-      for (var filei in files) {
+      for (var filei in _files) {
         if (filei.id == id) {
-          files.remove(filei);
+          setState(() {
+            _files.remove(filei);
+          });
           print('file with id: $id removed!');
           break;
         }
@@ -47,35 +48,10 @@ class _FileListViewState extends State<FileListView> {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        ...files.map(((file) => Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                alignment: Alignment.center,
-                height: 80,
-                padding: const EdgeInsets.all(15),
-                width: double.infinity,
-                color: const Color(0xFF864921),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      file.name ?? 'No file found',
-                      style: const TextStyle(fontSize: 24),
-                    ),
-                    IconButton(
-                        onPressed: () {
-                          setState(() {
-                            removeFile(file: file);
-                          });
-                        },
-                        icon: const Icon(
-                          Icons.close,
-                          color: Colors.red,
-                        )),
-                  ],
-                ),
-              ),
-            ))),
+        ..._files.map((file) => FileInfo(
+              file: file,
+              deleteFunc: removeFile,
+            )),
       ],
     );
   }
