b7819ae17dd5edf273dadee091262c41172da578
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-06-16 14:28:56 +0300

    wrap controller with class to pass through reference

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,11 +4,12 @@ import '../widgets/text_field.dart';
 import '../widgets/my_button.dart';
 import '../widgets/app_bar.dart';
 import '../style/colors.dart';
+import '../models/controller.dart';
 
 class HomePage extends StatelessWidget {
   static const pageName = '/';
 
-  final myController = TextEditingController();
+  final myController = MyController(TextEditingController());
   HomePage({super.key});
 
   @override
@@ -17,40 +18,43 @@ class HomePage extends StatelessWidget {
       appBar: getAppBar(),
       body: Container(
         color: colorPrimaryRedCaramel,
-        child: Column(
-          children: [
-            MyTextField(
-              myController: myController,
-              maxLines: 10,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                MyButton(
-                  text: 'Upload',
-                  onPressed: () {
-                    Navigator.pushNamed(
-                      context,
-                      '/upload_file',
-                    );
-                  },
-                ),
-                MyButton(
-                  text: 'Fix',
-                  onPressed: () {
-                    Navigator.pushNamed(
-                      context,
-                      '/fix_text',
-                      arguments: myController.text,
-                    );
-                  },
-                ),
-              ],
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              MyTextField(
+                myController: myController,
+                maxLines: 20,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  MyButton(
+                    text: 'Upload',
+                    onPressed: () {
+                      Navigator.pushNamed(
+                        context,
+                        '/upload_file',
+                      );
+                    },
+                  ),
+                  MyButton(
+                    text: 'Fix',
+                    onPressed: () {
+                      Navigator.pushNamed(
+                        context,
+                        '/fix_text',
+                        arguments: myController,
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
