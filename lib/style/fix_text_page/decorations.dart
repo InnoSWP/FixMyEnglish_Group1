4fb1a476596003fe6cbee0e9c60a4db0b091e33e
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-06-26 23:23:35 +0300

    feat: new style files for fix_text page

---
+++ lib/style/fix_text_page/decorations.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import './colors.dart';
+
+const BoxDecoration decorationBlocks = BoxDecoration(
+  color: backgroundBlocks,
+  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+);
