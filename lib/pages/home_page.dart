f8364080f70525be6d3cd74615d1bb52dd831b22
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-06-26 23:22:17 +0300

    fix: home page changed to fix_text

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,155 +1,123 @@
-import 'package:fix_my_english/constants/constants.dart';
-import 'package:fix_my_english/widgets/file_list.dart';
+import 'package:fix_my_english/widgets/text_field.dart';
 import 'package:flutter/material.dart';
 
-import '../widgets/text_field.dart';
-import '../widgets/my_button.dart';
+import '../style/fix_text_page/text_style.dart';
+import '../widgets/mistake_sentence.dart';
 import '../widgets/app_bar.dart';
-import '../style/colors.dart';
+import '../widgets/my_button.dart';
+import '../services/api_service.dart';
 import '../models/controller.dart';
+import '../models/sentence.dart';
+import '../widgets/mistake_list.dart';
+import '../style/colors.dart';
+import '../style/fix_text_page/decorations.dart';
+import '../style/fix_text_page/button_style.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   static const pageName = '/';
 
-  final myController = MyController(TextEditingController());
-  HomePage({super.key});
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  static final _sentences = [];
+
+  void _addSentences(Sentence s) {
+    setState(() {
+      _sentences.add(MistakeSentence(
+        text: s.text,
+        suggestion: s.suggestion,
+        error: s.error,
+      ));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    var args = MyController(TextEditingController(text: ''));
+    if (ModalRoute.of(context)?.settings.arguments != null) {
+      args = ModalRoute.of(context)!.settings.arguments as MyController;
+    }
     return Scaffold(
-      appBar: getAppBar(),
-      body: Container(
-        color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Row(children: [
-                Container(
-                  width: 1000,
-                  height: 400,
-                  margin: EdgeInsets.all(10.0),
-                  decoration: BoxDecoration(
-                    color: Color(0xFFF2EEE1),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Row(
-                    children: [
-                      Container(
-                        width: 250,
-                        height: 400,
-                        child: Align(
-                          alignment: Alignment.bottomLeft,
-                          child: SizedBox(
-                            width: 150,
-                            height: 50,
-                            child: Container(
-                                child: Center(
-                                  child: Text(
-                                    'Export CSV',
-                                    style: TextStyle(
-                                        fontFamily: 'Eczar',
-                                        color: Colors.white),
-                                  ),
-                                ),
-                                margin: EdgeInsets.all(10.0),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(8.0),
-                                  color: colorSecondaryGreenPlant,
-                                )),
-                          ),
-                        ),
-                      ),
-                      Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              height: 50,
-                            ),
-                            Image.asset(noPdfPath),
-                            Text(
-                              'No pdf imported',
-                              style: TextStyle(fontWeight: FontWeight.w700),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              'Please add pdf for iExtract to check',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w200,
-                                  color: Colors.grey),
-                            )
-                          ],
-                        ),
-                      ),
-                    ],
+      appBar: getAppBar(actions: true),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          const Divider(color: colorPrimaryRedCaramel, height: 3),
+          const Divider(color: colorPrimaryRedCaramel, height: 5),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    margin:
+                        const EdgeInsets.only(left: 20, top: 20, bottom: 20),
+                    decoration: decorationBlocks,
+                    child: MistakeList(sentences: _sentences),
                   ),
                 ),
                 Expanded(
-                    child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  flex: 1,
+                  child: Container(
+                    margin: const EdgeInsets.all(20),
+                    decoration: decorationBlocks,
+                    // alignment: Alignment.topCenter,
+                    child: Stack(
+                      alignment: Alignment.bottomCenter,
+                      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      // crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
-                        Expanded(
-                          child: Align(
-                            alignment: Alignment.topRight,
-                            child: SizedBox(
-                                width: 60,
-                                height: 40,
-                                child: Container(
-                                  child: Column(
-                                    children: [
-                                      Text(
-                                        'Add pdf',
-                                        style: TextStyle(color: Colors.green),
-                                      ),
-                                      SizedBox(
-                                        height: 3,
-                                      ),
-                                      Image.asset(plus)
-                                    ],
-                                  ),
-                                )),
+                        MyTextField(
+                          maxLines: 50,
+                          myController: args,
+                          text: args.controller.text,
+                          borderRadius: 20.0,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(bottom: 18),
+                          child: MyButton(
+                            onPressed: () {
+                              setState(() {
+                                _sentences.clear();
+                              });
+                              postTextSample(
+                                text: args.controller.text,
+                                context: context,
+                              ).then((l) {
+                                l.forEach(_addSentences);
+                              });
+                            },
+                            height: 50,
+                            width: 185,
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(20.0)),
+                            gradient: buttonLinearGradient,
+                            child: Row(
+                              children: [
+                                Image.asset('icons/analyze_icon.png'),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                const Text(
+                                  'Analyse text',
+                                  style: analyzeButton,
+                                ),
+                              ],
+                            ),
                           ),
                         ),
-                        Expanded(
-                          child: Align(
-                            alignment: Alignment.topLeft,
-                            child: SizedBox(
-                                width: 64,
-                                height: 40,
-                                child: Container(
-                                  child: Column(
-                                    children: [
-                                      Text(
-                                        'Delete pdf',
-                                        style: TextStyle(color: Colors.red),
-                                      ),
-                                      SizedBox(
-                                        height: 3,
-                                      ),
-                                      Image.asset(urnaPomoika)
-                                    ],
-                                  ),
-                                )),
-                          ),
-                        )
                       ],
                     ),
-                    Container(
-                        height: 400,
-                        child: Expanded(
-                          child: FileListView(),
-                        )),
-                  ],
-                )),
-              ]),
-            ],
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
