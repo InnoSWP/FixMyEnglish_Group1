d276d0ec0b4967f6f55f87902b904a0127780f33
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-06-28 13:02:16 +0300

    fix: delete not used colors

--- lib/style/colors.dart
+++ lib/style/colors.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
 
-// button background color
-const colorSecondaryGreenPlant = Color(0xFF4D6658);
-
 // background color of home page
 const colorPrimaryRedCaramel = Color(0xFF864921);
-
-// background color of list of mistakes box
-const colorSecondaryLightGreenPlant = Color(0xFFE9F1E8);
 
 // red color for incorrect sentence
 const customRed = Color.fromARGB(255, 255, 17, 0);
