99f87e4911d06c148037d6df8e11d597f65943f6
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-07-04 08:07:02 +0300

    feat: new more flexible CustomToast

--- lib/widgets/custom_toast.dart
+++ lib/widgets/custom_toast.dart
@@ -1,26 +1,94 @@
+import 'package:fix_my_english/style/decorations.dart';
 import 'package:flutter/material.dart';
+
+import '../style/text_style.dart';
+
+enum ToastType {
+  info,
+  warning,
+  error,
+}
 
 class CustomToast extends StatelessWidget {
   const CustomToast({
     super.key,
-    required this.child,
+    required this.msg,
+    this.type = ToastType.info,
   });
 
-  final Widget child;
+  final ToastType type;
+  final String msg;
 
   @override
   Widget build(BuildContext context) {
+    switch (type) {
+      case ToastType.info:
+        return _buildInfoType(context);
+      case ToastType.warning:
+        return _buildWarningType(context);
+      case ToastType.error:
+        return _buildErrorType(context);
+      default:
+        return const Center(
+          child: Text('Toast type not found!'),
+        );
+    }
+  }
+
+  Widget _buildInfoType(BuildContext context) {
     return Container(
       height: 50,
-      width: 300,
+      width: 320,
       alignment: Alignment.center,
       margin: const EdgeInsets.only(bottom: 30),
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 7),
-      decoration: BoxDecoration(
-        color: Color.fromARGB(255, 112, 106, 106),
-        borderRadius: BorderRadius.circular(100),
+      padding: const EdgeInsets.symmetric(vertical: 7),
+      decoration: infoBoxReport,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('icons/info_icon_report.png', height: 30, width: 30),
+          const SizedBox(width: 10),
+          Text(msg, style: successToast),
+        ],
       ),
-      child: child,
+    );
+  }
+
+  Widget _buildWarningType(BuildContext context) {
+    return Container(
+      height: 50,
+      width: 320,
+      alignment: Alignment.center,
+      margin: const EdgeInsets.only(bottom: 30),
+      padding: const EdgeInsets.symmetric(vertical: 7),
+      decoration: warningBoxReport,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('icons/warning_icon_report.png', height: 30, width: 30),
+          const SizedBox(width: 10),
+          Text(msg, style: warningToast),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildErrorType(BuildContext context) {
+    return Container(
+      height: 50,
+      width: 320,
+      alignment: Alignment.center,
+      margin: const EdgeInsets.only(bottom: 30),
+      padding: const EdgeInsets.symmetric(vertical: 7),
+      decoration: errorBoxReport,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('icons/error_icon_report.png', height: 30, width: 30),
+          const SizedBox(width: 10),
+          Text(msg, style: errorToast),
+        ],
+      ),
     );
   }
 }
