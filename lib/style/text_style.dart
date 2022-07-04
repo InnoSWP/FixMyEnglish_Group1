c0fec47241c0dd7eb0ced3ed0184bb4cc936c111
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-07-04 08:04:04 +0300

    feat: new text style for notifications[toasts]

--- lib/style/text_style.dart
+++ lib/style/text_style.dart
@@ -43,3 +43,21 @@ const TextStyle extractButtonStyle = TextStyle(
   fontSize: 18,
   fontFamily: 'Eczar',
 );
+
+const TextStyle errorToast = TextStyle(
+  fontSize: 18,
+  color: Colors.black,
+  overflow: TextOverflow.ellipsis,
+);
+
+const TextStyle successToast = TextStyle(
+  fontSize: 18,
+  color: Colors.black,
+  overflow: TextOverflow.ellipsis,
+);
+
+const TextStyle warningToast = TextStyle(
+  fontSize: 18,
+  color: Colors.black,
+  overflow: TextOverflow.ellipsis,
+);
