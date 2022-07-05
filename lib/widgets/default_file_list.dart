4045b93b75f37108c30098648fe46bbdc41f8666
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-07-05 13:17:26 +0300

    fix: remove unnecessary container

--- lib/widgets/default_file_list.dart
+++ lib/widgets/default_file_list.dart
@@ -7,28 +7,26 @@ class DefaultFileList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset("assets/icons/upload_file_not_imported.png"),
-          const SizedBox(height: 38),
-          const Text(
-            'No pdf imported',
-            style: TextStyle(
-              fontSize: 30,
-              fontFamily: 'Eczar',
-            ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset("assets/icons/upload_file_not_imported.png"),
+        const SizedBox(height: 38),
+        const Text(
+          'No pdf imported',
+          style: TextStyle(
+            fontSize: 30,
+            fontFamily: 'Eczar',
           ),
-          const SizedBox(height: 15),
-          const Text(
-            'Please add pdf for iExtract to check',
-            style: TextStyle(
-              color: noTextColor,
-            ),
+        ),
+        const SizedBox(height: 15),
+        const Text(
+          'Please add pdf for iExtract to check',
+          style: TextStyle(
+            color: noTextColor,
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
