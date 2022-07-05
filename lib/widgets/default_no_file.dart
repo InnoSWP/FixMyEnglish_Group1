2954ae748c8160c0c0a7061c9fa9b5cd2f7f61e9
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-07-05 14:22:12 +0300

    docs: init DefaultNoFile doc

--- lib/widgets/default_no_file.dart
+++ lib/widgets/default_no_file.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
+/// A widget to show when no file imported or all files are deleted.
+///
+/// This widget has a button that you can click and add new files
 class DefaultNoFile extends StatelessWidget {
+  /// Provide a [VoidCallback] function when 'add pdf' button clicked.
   final VoidCallback onPressed;
   const DefaultNoFile({super.key, required this.onPressed});
 
