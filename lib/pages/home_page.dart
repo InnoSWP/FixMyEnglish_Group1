d9145f744cdcd041558df4681744b6e3fb55ca00
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-07-05 13:12:22 +0300

    fix: move implementation of button to /utilities/analyse_text_button.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,13 @@
-import 'dart:io';
-
-import 'package:fix_my_english/widgets/text_field.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 
+import '../constants/constants.dart';
+import '../style/fix_text_page/colors.dart';
+import '../utilities/analyse_text_button.dart';
+import '../widgets/text_field.dart';
 import '../style/fix_text_page/text_style.dart';
-import '../widgets/custom_loading.dart';
-import '../widgets/custom_toast.dart';
-import '../widgets/mistake_sentence.dart';
 import '../widgets/app_bar.dart';
 import '../widgets/my_button.dart';
-import '../services/api_service.dart';
 import '../models/controller.dart';
-import '../models/sentence.dart';
 import '../widgets/mistake_list.dart';
 import '../style/colors.dart';
 import '../style/fix_text_page/decorations.dart';
@@ -31,22 +26,11 @@ class _HomePageState extends State<HomePage> {
   final myController = MyController(TextEditingController());
   static final _sentences = [];
 
-  void _addSentences(Sentence s) {
-    setState(() {
-      _sentences.add(MistakeSentence(
-        label: s.label,
-        text: s.text,
-        suggestion: s.suggestion,
-        error: s.error,
-      ));
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: getAppBar(actions: true),
-      backgroundColor: Colors.white,
+      backgroundColor: backgroundPageColor,
       body: Column(
         children: [
           const Divider(color: colorPrimaryRedCaramel, height: 3),
@@ -61,11 +45,12 @@ class _HomePageState extends State<HomePage> {
                         const EdgeInsets.only(left: 20, top: 20, bottom: 20),
                     decoration: decorationBlocks,
                     child: MistakeList(
-                        fileName: "yourFileName", sentences: _sentences),
+                      fileName: defaultFileName,
+                      sentences: _sentences,
+                    ),
                   ),
                 ),
                 Expanded(
-                  flex: 1,
                   child: Container(
                     margin: const EdgeInsets.all(20),
                     decoration: decorationBlocks,
@@ -75,7 +60,6 @@ class _HomePageState extends State<HomePage> {
                         MyTextField(
                           maxLines: 50,
                           myController: myController,
-                          text: myController.controller.text,
                           borderRadius: 20.0,
                         ),
                         Container(
@@ -83,31 +67,12 @@ class _HomePageState extends State<HomePage> {
                           margin: const EdgeInsets.only(bottom: 18),
                           child: MyButton(
                             onPressed: () async {
-                              SmartDialog.showLoading(builder: (context) {
-                                return const CustomLoading();
-                              });
-
-                              setState(() {
-                                _sentences.clear();
-                              });
-                              try {
-                                (await postText(
-                                  text: myController.controller.text,
-                                  context: context,
-                                ))
-                                    .forEach(_addSentences);
-                              } catch (e) {
-                                print('Error: $e');
-                                SmartDialog.showToast('',
-                                    alignment: Alignment.bottomCenter,
-                                    builder: (context) {
-                                  return const CustomToast(
-                                    msg: 'Failed to fetch data from API',
-                                    type: ToastType.error,
-                                  );
-                                });
-                              }
-                              SmartDialog.dismiss();
+                              await analyseTextButton(
+                                context: context,
+                                myController: myController,
+                                sentences: _sentences,
+                              );
+                              setState(() {});
                             },
                             height: 50,
                             width: 185,
