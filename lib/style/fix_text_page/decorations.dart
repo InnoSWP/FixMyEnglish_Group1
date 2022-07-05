5f8cb45c0a5d581b523a3e0baf11cddf0de39eb5
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-07-05 13:14:14 +0300

    fix: add const to constant constructor

--- lib/style/fix_text_page/decorations.dart
+++ lib/style/fix_text_page/decorations.dart
@@ -4,13 +4,13 @@ import './colors.dart';
 
 final BoxDecoration decorationBlocks = BoxDecoration(
   boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: Offset(0, 3), // changes position of shadow
-                  ),
-                ],
+    BoxShadow(
+      color: Colors.grey.withOpacity(0.5),
+      spreadRadius: 5,
+      blurRadius: 7,
+      offset: const Offset(0, 3), // changes position of shadow
+    ),
+  ],
   color: backgroundBlocks,
   borderRadius: const BorderRadius.all(Radius.circular(20.0)),
   border: Border.all(color: Colors.black38),
