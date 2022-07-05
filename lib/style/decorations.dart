3101d84fb0abe137ac41362d1b242595c160873b
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-07-05 13:13:43 +0300

    fix: add const to constant constructors

--- lib/style/decorations.dart
+++ lib/style/decorations.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
 final BoxDecoration infoBoxReport = BoxDecoration(
-  color: Color.fromARGB(255, 156, 210, 253),
+  color: const Color.fromARGB(255, 156, 210, 253),
   borderRadius: BorderRadius.circular(100),
 );
 
 final BoxDecoration warningBoxReport = BoxDecoration(
-  color: Color.fromARGB(255, 255, 210, 143),
+  color: const Color.fromARGB(255, 255, 210, 143),
   borderRadius: BorderRadius.circular(100),
 );
 
 final BoxDecoration errorBoxReport = BoxDecoration(
-  color: Color.fromARGB(255, 255, 172, 166),
+  color: const Color.fromARGB(255, 255, 172, 166),
   borderRadius: BorderRadius.circular(100),
 );
