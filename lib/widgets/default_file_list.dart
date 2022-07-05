99feaee5e5044cb7e087ed6fa670ac58f0346558
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-07-05 14:15:48 +0300

    docs: init DefaultFileList docs

--- lib/widgets/default_file_list.dart
+++ lib/widgets/default_file_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../style/fix_text_page/colors.dart';
 
+/// A widget to show when no file chosen or imported.
 class DefaultFileList extends StatelessWidget {
   const DefaultFileList({super.key});
 
