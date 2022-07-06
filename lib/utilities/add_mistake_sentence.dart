838052a7477c54cdcdb1315451dc74db0800f9f9
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-07-06 13:41:59 +0300

    docs: init addSentence() doc

--- lib/utilities/add_mistake_sentence.dart
+++ lib/utilities/add_mistake_sentence.dart
@@ -1,6 +1,11 @@
 import '../models/sentence.dart';
 import '../widgets/mistake_sentence.dart';
 
+/// A function to add [sentence] to list [sentences].
+///
+/// This function simply takes data from [sentence] and
+/// by using [MistakeSentence] widget convert it into full working widget with
+/// hover on match, description of error etc.
 void addSentence(Sentence sentence, List sentences) {
   sentences.add(MistakeSentence(
     label: sentence.label,
