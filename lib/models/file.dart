a0f1d340e3bded192f0f5c48615380d25120a6fc
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-07-06 11:53:31 +0300

    docs: init File doc

--- lib/models/file.dart
+++ lib/models/file.dart
@@ -1,10 +1,21 @@
+/// A class that represents a single [File]
+///
+/// From this class you can take [name] of file.
+/// Identifier [id] of a certain file.
+/// And all [mistakeSentences] that was sent by API.
 class File {
-  String? name;
-  int id = 0;
-  final List mistakeSentences;
   File({
     required this.name,
     required this.id,
     this.mistakeSentences = const [],
   });
+
+  /// Name of a [File].
+  String? name;
+
+  /// Identifier of a [File].
+  int id = 0;
+
+  /// All mistakes from [File] sent by API.
+  final List mistakeSentences;
 }
