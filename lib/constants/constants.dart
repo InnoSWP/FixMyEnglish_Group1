5cc07e14b0e5cc591e767ae3d7b4b97938dafa85
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-07-10 14:02:17 +0300

    docs: init doc

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,16 +1,26 @@
 const logoPath = "assets/logo/logo.png";
 
+/// API url that we use, to detect mistake sentences.
 const apiUrl = 'https://aqueous-anchorage-93443.herokuapp.com/FixMyEnglish';
 
+/// [AllowedExtension] for uploaded files while uploading with `upload button`.
 const allowedExtensions = <String>['pdf'];
 
+/// Save last [saveNReports] reports made by user,
+/// if user will try to make similar one ignore his report.
 const int saveNReports = 100;
 
+/// Minimum reporting difference between last two reports.
 const limitToMakeQueryDB = 1000; // milliseconds
 
+/// [defaultFileName] for Home Page, while extracting csv file.
 const defaultFileName = 'yourFileName';
 
+/// [allowMultipleFileUpload] in Upload File Page with `upload button`.
 const allowMultipleFileUpload = true;
+
+/// [allowDragAndDrop] in Upload File Page.
+const allowDragAndDrop = true;
 
 // translate label to -> link, height, width of its icon
 const labelToIconAsset = <String, Map<String, Object>>{
