9f7a25acf1b31a155a2d597f2f96fb4330818e9b
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-07-10 14:02:36 +0300

    docs: init docs

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../constants/constants.dart';
+import '../style/decorations.dart';
 import '../style/fix_text_page/colors.dart';
 import '../utilities/analyse_text_button.dart';
 import '../widgets/text_field.dart';
@@ -10,9 +11,41 @@ import '../widgets/my_button.dart';
 import '../models/controller.dart';
 import '../widgets/mistake_list.dart';
 import '../style/colors.dart';
-import '../style/fix_text_page/decorations.dart';
-import '../style/fix_text_page/button_style.dart';
 
+/// ### The Main Page of [FixMyEnglish](https://fix-my-english-c6270.web.app/).
+///
+/// Home page consist of [MistakeList] and [MyTextField].
+///
+/// [MyTextField] - is where you can type a text then press `Analyse text`
+/// button to send your text to API, after receiving from API results it will show all
+/// the mistakes in [MistakeList].
+/// Empty [MyTextField] will be ignored.
+///
+/// [MistakeList] - list of mistakes received from API, it will show all mistakes and
+/// by hover on error phrases there appears a box where you see a description for an error.
+/// if you think that error that was provided by API is wrong(false-positive) you can send
+/// report by clicking red button near each sentence.
+///
+/// There also button in [AppBar] called `Bulk Upload`, where by clicking
+/// you will be directed to next page(Upload File Page).
+///
+/// #### Toasts(Message, Notification) that you can get:
+/// 1. INFO - type
+///   - `Connected to API` - successfully connected to the API, congratulations!
+///   - `No mistake found!` - Your text was sent to API, and API didn't detect any mistakes, lucky you!
+///   - `Report sent, thanks!` - Your report was successfully recorded to FireStore, thanks again :)
+/// 2. WARNING - type
+///   - `Connected to Mock API` - Connected to Mock API which is only for displaying some predefined data,
+/// you can disable it in [analyseTextButton] pass parameter `connectMock:false` in postText() function.
+/// 3. ERROR - type
+///   - `Failed to connect to API` - API is not available or there is some other problem with API or hosting.
+///   - `No reason for bug provided` - Trying to send a report which has no reason.
+///   - `Failed with status code: $statusCode!` - Trying connect to API, but API sent statusCode different from `200`.
+///   - `Denied, you made such report!` - if you are trying to send report which is similar to one of [saveNReports] last reports.
+///   - `Denied, too many reports!` - if your difference between your two last reports is less than [limitToMakeQueryDB] milliseconds.
+///   - `Failed to record your report!` - if something will go wrong while trying to send your report to firebase
+///   - `Failed to fetch data from API` - if something will go wrong after pressing `Analyse text`
+///   - `Can't find provided label!` - if API sent a label which is not add in [labelToIconAsset].
 class HomePage extends StatefulWidget {
   static const pageName = '/';
 
@@ -23,7 +56,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  /// Controller for the [MyTextField], for taking the text you typed.
   final myController = MyController(TextEditingController());
+
+  /// List of sentences that [MistakeList] will have.
   static final _sentences = [];
 
   @override
