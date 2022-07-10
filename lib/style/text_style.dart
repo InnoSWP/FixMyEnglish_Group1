203e9e89efb72f71fd647145efbfad4ec964a776
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-07-10 14:03:46 +0300

    docs: init docs

--- lib/style/text_style.dart
+++ lib/style/text_style.dart
@@ -2,61 +2,50 @@ import 'package:flutter/material.dart';
 
 import './colors.dart';
 
+/// [TextStyle] for mistake phrases in MistakeList.
 const TextStyle mistakeSentence = TextStyle(
   color: customRed,
   fontSize: 20,
   overflow: TextOverflow.visible,
 );
 
+/// [TextStyle] for all sentences in MistakeList except error phrases.
 const TextStyle allSentence = TextStyle(
   color: customBlack,
   fontSize: 20,
   overflow: TextOverflow.visible,
 );
 
+/// [TextStyle] for mistake phrases in report a bug box.
 const TextStyle smallMistakeSentence = TextStyle(
   color: customRed,
   fontSize: 16,
   overflow: TextOverflow.visible,
 );
 
+/// [TextStyle] for all sentences in report a bux box except error phrases.
 const TextStyle smallAllSentence = TextStyle(
   color: customBlack,
   fontSize: 16,
   overflow: TextOverflow.visible,
 );
 
-const BoxDecoration suggestionDecoration = BoxDecoration(
-  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-  color: customBlue,
-);
-
-// file info text style
+/// [TextStyle] for displaying some information about a file. e.g file name
 const TextStyle fileInfoStyle = TextStyle(
   color: customBlack,
   fontSize: 24,
   overflow: TextOverflow.ellipsis,
 );
 
+/// [TextStyle] for text in extract buttons.
 const TextStyle extractButtonStyle = TextStyle(
   color: Colors.white,
   fontSize: 18,
   fontFamily: 'Eczar',
 );
 
-const TextStyle errorToast = TextStyle(
-  fontSize: 18,
-  color: Colors.black,
-  overflow: TextOverflow.ellipsis,
-);
-
-const TextStyle successToast = TextStyle(
-  fontSize: 18,
-  color: Colors.black,
-  overflow: TextOverflow.ellipsis,
-);
-
-const TextStyle warningToast = TextStyle(
+/// [TextStyle] for a message in Toasts.
+const TextStyle toastTextStyle = TextStyle(
   fontSize: 18,
   color: Colors.black,
   overflow: TextOverflow.ellipsis,
