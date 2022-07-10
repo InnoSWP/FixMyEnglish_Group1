b793200373b372bc0039a16e88a6d18460daf1b4
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-07-10 14:03:59 +0300

    docs: init docs

--- lib/style/fix_text_page/text_style.dart
+++ lib/style/fix_text_page/text_style.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
+/// TextStyle for button called `Bulk Upload` which redirects you to Upload Page.
 const TextStyle uploadButton = TextStyle(
   color: Colors.black,
   fontSize: 22,
   fontFamily: 'Eczar',
 );
 
+/// TextStyle for Buttons.
+///
+/// Usage:
+///   `Analyse text` - button in Home Page.
 const TextStyle analyzeButton = TextStyle(
   color: Colors.white,
   fontSize: 18,
