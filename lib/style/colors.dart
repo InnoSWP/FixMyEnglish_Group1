f73f04dede3120d285c196bae9bf8ea90db0b950
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-07-10 14:03:30 +0300

    docs: init docs

--- lib/style/colors.dart
+++ lib/style/colors.dart
@@ -1,27 +1,51 @@
 import 'package:flutter/material.dart';
 
-// background color of home page
+/// Background color.
+///
+/// Usage:
+///   [Divider] - color in Home Page and Upload Page.
 const colorPrimaryRedCaramel = Color(0xFF864921);
 
-// red color for incorrect sentence
+/// Red color to highlight the mistake phrase.
+///
+/// Usage:
+///   [TextStyle] - for mistake sentence.
 const customRed = Color.fromARGB(255, 255, 17, 0);
 
-// black color for all sentence
+/// Black color for all other text in MistakeList.
+///
+/// Usage:
+///   [TextStyle] - for sentences in MistakeList, not including error phrases.
 const customBlack = Color.fromARGB(255, 0, 0, 0);
 
-// background color of suggestion box
-const customBlue = Color.fromARGB(255, 174, 211, 241);
-
-// main color of all blocks
+/// Main color of all containers in our program
+///
+/// Usage:
+///   [BoxDecoration] - for containers like MistakeList, MyTextField, FileListView.
 const Color colorTextbackgroundtextbox = Color(0xFFF2EEE1);
 
-// background color of button[extract]
+/// Background color of button.
+///
+/// Usage:
+///   `Extract to csv` - button in Home Page and Upload Page.
+///   `Extract all to csv` - button in Upload Page.
+///   `Send report` - button for reporting a bug.
 const backgroundButton = Color(0xFF4D6658);
 
+/// Background color of Container.
+///
+/// Usage:
+///   [AlertDialog] - for reporting a bug.
 const backgroundReportBug = Color(0xFFF2F2F2);
 
+/// Background color of buttons while trying to Drop a file.
 const onHoverDragDropButton = Color(0xFF82490D);
 
+/// Background color of [AppBar].
 const appBarBackground = Colors.white;
 
+/// Background color of logo in [AppBar].
 const appBarIconBackground = Color(0xFF864921);
+
+/// Background color of Home Page and Upload Page.
+const backgroundPageColor = Colors.white;
