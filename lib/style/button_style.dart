6d6af8e8c402094279de043b6a366d9372ab9600
Toirov Sadi <sadi.toirov2003@gmail.com>
2022-07-10 14:03:22 +0300

    docs: init docs

---
+++ lib/style/button_style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+/// ElevatedButton style for button with gradient.
+final ButtonStyle gradientButtonStyle = ElevatedButton.styleFrom(
+  backgroundColor: Colors.transparent,
+  shadowColor: Colors.transparent,
+);
